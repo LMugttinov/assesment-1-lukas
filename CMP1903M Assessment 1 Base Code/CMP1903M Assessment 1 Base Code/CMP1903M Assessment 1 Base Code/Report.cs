d150fddea02eae6b1f41fffadfe8e45506dd9a47
LMugttinov <102603950+LMugttinov@users.noreply.github.com>
2022-03-29 23:35:31 +0100

    Update Report.cs

--- CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Report.cs
+++ CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Report.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CMP1903M_Assessment_1_Base_Code.Input;
+using static CMP1903M_Assessment_1_Base_Code.Analyse;
+using static CMP1903M_Assessment_1_Base_Code.Report;
 
 namespace CMP1903M_Assessment_1_Base_Code
 {
