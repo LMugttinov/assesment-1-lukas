b817e9cdc58edd19c2da1f8932af4581b0e12363
LMugttinov <102603950+LMugttinov@users.noreply.github.com>
2022-03-29 23:33:36 +0100

    Update Program.cs

--- CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Program.cs
+++ CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Program.cs
@@ -4,48 +4,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CMP1903M_Assessment_1_Base_Code.Input;
+using static CMP1903M_Assessment_1_Base_Code.Analyse;
+using static CMP1903M_Assessment_1_Base_Code.Report;
+
 
 namespace CMP1903M_Assessment_1_Base_Code
 {
     class Program
     {
-        static void Main()
+        public static void Main()
         {
+            Input input = new Input();
+            Analyse analyse = new Analyse();
+
             //define result integer
-            int Result;
+            string Result;
             //define a string that will be used for analysis
-            string Str;
+            string str;
+            //define a file that will turn into a string
+            string file;
+
             //gives the user the choice between the following
             Console.WriteLine("To pick your own TEXT being analysed please type 1");
             Console.WriteLine("To pick your own FILE being analysed please type 2");
             //result is assigned
-            Result = Console.Readline();
+            Result = Console.ReadLine();
 
             //allows user to input any text if 1 was selected
-            if (Result == 1)
+            if (Result == ("1"))
             {
-                Str = input.ManualTextInput();
-                Analyse.AnalyseText(Str);
+                 str = input.manualTextInput();
+                = analyse.AnalyseText(str);
+                
             }
-            else if (Result == 2)
+            //locates file
+            else if (Result == ("2"))
             {
-                Console.WriteLine("What is the exact name of the file?")
-                string file = Console.ReadLine();
-                Str = input.FileTextInput(file)
-                Analyse.AnalyseText(Str);
+                Console.WriteLine("File Found");
+                str = input.fileTextInput("CMP1903M Assessment 1 Test File.txt");
+                 = analyse.AnalyseText(str);
             }
+            //if user used different characters outside of 1 or 2
             else
             {
-                Console.WriteLine("please only enter values of 1 or 2!")
+                Console.WriteLine("please only enter values of 1 or 2!");
             }
-
-
-            //Get either manually entered text, or text from a file
-
-
-            //Create an 'Analyse' object
-            //Pass the text input to the 'analyseText' method
-
 
             //Receive a list of integers back
 
