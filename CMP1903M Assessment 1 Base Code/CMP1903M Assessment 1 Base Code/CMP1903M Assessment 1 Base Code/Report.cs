ddda9952e311e323eef4a5286fb9f6df9e26055a
LMugttinov <102603950+LMugttinov@users.noreply.github.com>
2022-03-29 20:50:59 +0100

    Update Report.cs

--- CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Report.cs
+++ CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Report.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,15 @@ namespace CMP1903M_Assessment_1_Base_Code
         //Handles the reporting of the analysis
         //Maybe have different methods for different formats of output?
         //eg.   public void outputConsole(List<int>)
+        public void outputConsole(List<int> input)
+        {
+            Console.WriteLine("Number of sentences: " + input[0]);
+            Console.WriteLine("Number of vowels: " + input[1]);
+            Console.WriteLine("Number of consonants: " + input[2]);
+            Console.WriteLine("Number of upper case letters: " + input[3]);
+            Console.WriteLine("Number of lower case letters: " + input[4]);
+            Console.WriteLine("Number of Characters: " + input[5]);
 
+        }
     }
 }
