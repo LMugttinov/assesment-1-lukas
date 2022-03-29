a70d801309b18474aaa89cdddd8e2a158bf48043
LMugttinov <102603950+LMugttinov@users.noreply.github.com>
2022-03-29 20:18:47 +0100

    Update Input.cs

--- CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Input.cs
+++ CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/CMP1903M Assessment 1 Base Code/Input.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +9,7 @@ namespace CMP1903M_Assessment_1_Base_Code
     public class Input
     {
         //Handles the text input for Assessment 1
-        string text = "nothing";
+        string str = "nothing";
         
         //Method: manualTextInput
         //Arguments: none
@@ -17,8 +17,10 @@ namespace CMP1903M_Assessment_1_Base_Code
         //Gets text input from the keyboard
         public string manualTextInput()
         {
-
-            return text;
+            Console.WriteLine("Enter text... ")
+            str = Console.Readline();
+            analyse.analyseText(str);
+            return str;
         }
 
         //Method: fileTextInput
@@ -27,8 +29,8 @@ namespace CMP1903M_Assessment_1_Base_Code
         //Gets text input from a .txt file
         public string fileTextInput(string fileName)
         {
-
-            return text;
+            str = File.ReadAllText(fileName)
+            return str;
         }
 
     }
